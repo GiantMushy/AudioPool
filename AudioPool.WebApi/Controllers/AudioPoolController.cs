1388fd6eee19c37556c7fe8a0df5b0b34cb85990
Þorri Bjarnason <thorvardur23@ru.is>
2025-09-18 11:14:08 +0000

    Descriptive namse to Controller functions

--- AudioPool.WebApi/Controllers/AudioPoolController.cs
+++ AudioPool.WebApi/Controllers/AudioPoolController.cs
@@ -10,56 +10,56 @@ public class AudioPoolController : ControllerBase
     // --------- Unauthorized access ---------
 
     // http://localhost:5000/audiopool/genres
-    [HttpGet("genres")]
+    [HttpGet("genres", Name = "GetAllGenres")]
     public ActionResult GetAllGenres()
     {
         return Ok("Hello from AudioPoolController");
     }
 
     // http://localhost:5000/audiopool/genres/1
-    [HttpGet("genres/{id:int}")]
+    [HttpGet("genres/{id:int}", Name = "GetGenreById")]
     public ActionResult GetGenreById(int id)
     {
         return Ok(id);
     }
 
     // http://localhost:5000/audiopool/artists
-    [HttpGet("artists")]
+    [HttpGet("artists", Name = "GetAllArtists")]
     public ActionResult GetAllArtists()
     {
         return Ok();
     }
 
     // http://localhost:5000/audiopool/artists/1
-    [HttpGet("artists/{id:int}")]
+    [HttpGet("artists/{id:int}", Name = "GetArtistById")]
     public ActionResult GetArtistById(int id)
     {
         return Ok(id);
     }
 
     // http://localhost:5000/audiopool/artists/1/albums
-    [HttpGet("artists/{id:int}/albums")]
-    public ActionResult GetArtistAlbums(int id)
+    [HttpGet("artists/{id:int}/albums", Name = "GetAlbumsByArtistId")]
+    public ActionResult GetAlbumsByArtistId(int id)
     {
         return Ok(id);
     }
 
     // http://localhost:5000/audiopool/albums/1
-    [HttpGet("albums/{id:int}")]
+    [HttpGet("albums/{id:int}", Name = "GetAlbumById")]
     public ActionResult GetAlbumById(int id)
     {
         return Ok(id);
     }
 
     // http://localhost:5000/audiopool/albums/1/songs
-    [HttpGet("albums/{id:int}/songs")]
-    public ActionResult GetAlbumSongs(int id)
+    [HttpGet("albums/{id:int}/songs", Name = "GetSongsByAlbumId")]
+    public ActionResult GetSongsByAlbumId(int id)
     {
         return Ok(id);
     }
 
     // http://localhost:5000/audiopool/songs/1
-    [HttpGet("songs/{id:int}")]
+    [HttpGet("songs/{id:int}", Name = "GetSongById")]
     public ActionResult GetSongById(int id)
     {
         return Ok(id);
@@ -69,63 +69,63 @@ public class AudioPoolController : ControllerBase
     // --------- Authorized access ---------
     // -------------------------------------
 
-    [HttpPost("genres")]
-    public ActionResult CreateNewGenre([FromBody] GenreDto input)
+    [HttpPost("genres", Name = "CreateGenre")]
+    public ActionResult CreateGenre([FromBody] GenreDto input)
     {
         return Ok(input);
     }
 
     //Create Artist
-    [HttpPost("artists")]
-    public ActionResult CreateNewArtist([FromBody] ArtistDto input)
+    [HttpPost("artists", Name = "CreateArtist")]
+    public ActionResult CreateArtist([FromBody] ArtistDto input)
     {
         return Ok(input);
     }
 
     //Update Artist
-    [HttpPut("artists/{id:int}")]
+    [HttpPut("artists/{id:int}", Name = "UpdateArtist")]
     public ActionResult UpdateArtist(int id)
     {
         return NoContent();
     }
 
     //Link Artist to Genre
-    [HttpPatch("artists/{artistId:int}/genres/{genreId:int}")]
+    [HttpPatch("artists/{artistId:int}/genres/{genreId:int}", Name = "LinkArtistToGenre")]
     public ActionResult LinkArtistToGenre(int artistId, int genreId)
     {
         return NoContent();
     }
 
     //Create Album
-    [HttpPost("artists/{id:int}/albums")]
-    public ActionResult CreateNewAlbum(int id)
+    [HttpPost("artists/{id:int}/albums", Name = "CreateAlbum")]
+    public ActionResult CreateAlbum(int id)
     {
         return Created();
     }
 
     //Delete Album
-    [HttpDelete("albums/{id:int}")]
+    [HttpDelete("albums/{id:int}", Name = "DeleteAlbum")]
     public ActionResult DeleteAlbum(int id)
     {
         return NoContent();
     }
 
     //Create Song
-    [HttpPost("albums/{id:int}/songs")]
-    public ActionResult CreateNewSong([FromBody] SongDto input)
+    [HttpPost("albums/{id:int}/songs", Name = "CreateSong")]
+    public ActionResult CreateSong([FromBody] SongDto input)
     {
         return Ok(input);
     }
 
     //Delete Song
-    [HttpDelete("songs/{id:int}")]
+    [HttpDelete("songs/{id:int}", Name = "DeleteSong")]
     public ActionResult DeleteSong(int id)
     {
         return NoContent();
     }
 
     //Update Song
-    [HttpPut("songs/{id:int}")]
+    [HttpPut("songs/{id:int}", Name = "UpdateSong")]
     public ActionResult UpdateSong(int id)
     {
         return NoContent();
